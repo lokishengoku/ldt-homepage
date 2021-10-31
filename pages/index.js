28ae36cce0f2380662e49bec0828444a2e23d88c
lokishengoku <dthuanle.20@gmail.com>
2021-11-01 00:45:15 +0700

    add bio

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
+import NextLink from 'next/link'
 import {
   Container,
   Box,
   Heading,
+  Button,
   Image,
   useColorModeValue
 } from '@chakra-ui/react'
+import { ChevronRightIcon } from '@chakra-ui/icons'
 import Section from '../components/section'
+import Paragraph from '../components/paragraph'
+import { BioSection, BioYear } from '../components/bio'
 
 const Page = () => {
   return (
@@ -49,7 +54,45 @@ const Page = () => {
         <Heading as={'h3'} variant={'section-title'}>
           Work
         </Heading>
-        <p>Paragraph</p>
+        <Paragraph>
+          I&apos;m a mobile developer based in Vietnam with a passion for making
+          interesting mobile applications and many other digital stuffs I want.
+          I have several projects that have been made with many
+          languages/frameworks such as Android Java, React Native, Flutter, Java
+          Swing, Arduino C/C++, ... In my free time, I love reading novels and
+          watching MMA/Boxing. Currently, I&apos;m spending my time to improve
+          my Flutter skill.
+        </Paragraph>
+        <Box align={'center'} my={4}>
+          <NextLink href={'/works'}>
+            <Button rightIcon={<ChevronRightIcon />} colorScheme={'teal'}>
+              My Portfolio
+            </Button>
+          </NextLink>
+        </Box>
+      </Section>
+      <Section delay={0.2}>
+        <Heading as={'h3'} variant={'section-title'}>
+          Bio
+        </Heading>
+
+        <BioSection>
+          <BioYear>2000</BioYear>
+          Born in Hatinh, Vietnam.
+        </BioSection>
+        <br />
+        <BioSection>
+          <BioYear>2018 - Present</BioYear>
+          Studying Information Technology (Engineering Degree) at Danang
+          University of Science
+        </BioSection>
+        <Heading as={'h3'} variant={'section-title'}>
+          I ❤
+        </Heading>
+        <Paragraph>
+          Reading, Playing, Learning, Coding, Novel, Rocket, Mechanical Stuff,
+          ...
+        </Paragraph>
       </Section>
     </Container>
   )
