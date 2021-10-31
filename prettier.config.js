bd26d7c45ea3766199ad1b077ce446742eb142fd
lokishengoku <dthuanle.20@gmail.com>
2021-10-31 20:30:15 +0700

    config

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+const options = {
+  arrowParens: 'void',
+  singleQuote: true,
+  bracketSpacing: true,
+  endOfline: 'lf',
+  semi: false,
+  tabWidth: 2,
+  trailingComma: 'none'
+}
+
+module.exports = options
