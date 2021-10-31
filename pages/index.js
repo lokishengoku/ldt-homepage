09ff09bcea4c0efbb3a857e072b03e70066bc043
lokishengoku <dthuanle.20@gmail.com>
2021-11-01 00:11:11 +0700

    add profile image & animated profile sections

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,58 @@
-import { Container, Box, Heading } from '@chakra-ui/react'
+import {
+  Container,
+  Box,
+  Heading,
+  Image,
+  useColorModeValue
+} from '@chakra-ui/react'
+import Section from '../components/section'
 
 const Page = () => {
-  return <Container>
-    <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
-      Hello, I&apos;m a mobile developer based in Vietnam!
-    </Box>
-
-    <Box display={{md: 'flex'}}>
-      <Box flexGrow={1}>
-        <Heading as="h2" variant={"page-title"}>
-          Thuan Le Duc
-        </Heading>
-        <p>CodeEye ( Android, React Native, Flutter )</p>
+  return (
+    <Container>
+      <Box
+        borderRadius="lg"
+        bg={useColorModeValue('whiteAlpha.600', 'whiteAlpha.300')}
+        p={3}
+        mb={6}
+        align="center"
+      >
+        Hello, I&apos;m a mobile developer based in Vietnam!
       </Box>
-    </Box>
-  </Container>
+
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant={'page-title'}>
+            Thuan Le Duc
+          </Heading>
+          <p>CodeEye ( Android, React Native, Flutter )</p>
+        </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          align={'center'}
+        >
+          <Image
+            borderColor={'whiteAlpha.800'}
+            borderWidth={2}
+            borderStyle={'solid'}
+            maxWidth={'100px'}
+            display={'inline-block'}
+            borderRadius={'full'}
+            src={'/images/ldt_face.jpg'}
+            alt={'Profile Image'}
+          />
+        </Box>
+      </Box>
+      <Section delay={0.1}>
+        <Heading as={'h3'} variant={'section-title'}>
+          Work
+        </Heading>
+        <p>Paragraph</p>
+      </Section>
+    </Container>
+  )
 }
 
 export default Page
